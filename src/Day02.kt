871ded8220eab816eca728bb442810f15a0f3b95
Jeanpierre <laurent.jeanpierre@unicaen.fr>
2024-12-02 08:37:12 +0100

    feat: day 02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,18 +1,68 @@
 fun main() {
+    var increasing : Boolean?
+    fun checkLine(levels: List<String>): Boolean {
+        increasing = null
+        var safe = true
+        levels.windowed(2).forEach { pair ->
+            val prev = pair[0].toInt()
+            val next = pair[1].toInt()
+            if (prev > next) {
+                if (increasing == true)
+                    safe = false
+                else {
+                    increasing = false
+                    if ((prev - next) !in 1..3)
+                        safe = false
+                }
+            } else if (prev == next) {
+                safe = false
+            } else {
+                if (increasing == false)
+                    safe = false
+                else {
+                    increasing = true
+                    if ((next - prev) !in 1..3)
+                        safe = false
+                }
+            }
+        }
+        return safe
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        input.forEach { line ->
+            val safe = checkLine(line.split(" "))
+            if (safe) count++
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0
+        input.forEach { line ->
+            val levels = line.split(" ")
+            var safe = checkLine(levels)
+            if (!safe) {
+                for( idx in levels.indices) {
+                    val newLevels = MutableList(levels.size-1) {
+                        return@MutableList levels[ if (it < idx) it else it+1 ]
+                    }
+                    if (checkLine(newLevels)) {
+                        safe = true
+                        break
+                    }
+                }
+            }
+            if (safe) count++
+        }
+        return count
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day02")
