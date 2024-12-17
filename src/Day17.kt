30b442a89889f84cb8608aab2cffb7a540b22b12
Jeanpierre <laurent.jeanpierre@unicaen.fr>
2024-12-17 13:59:01 +0100

    feat: day 17 - part2 failed

--- src/Day17.kt
+++ src/Day17.kt
@@ -1,21 +1,145 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    class Processor{
+        var pc = 0
+        var a = 0
+        var b = 0
+        var c = 0
+        val memory = mutableListOf<Int>()
+        val outValues = mutableListOf<Int>()
+
+        fun readInput(input: List<String>) {
+            a = Regex("Register A: (\\d+)").matchEntire(input[0])!!.groupValues[1].toInt()
+            b = Regex("Register B: (\\d+)").matchEntire(input[1])!!.groupValues[1].toInt()
+            c = Regex("Register C: (\\d+)").matchEntire(input[2])!!.groupValues[1].toInt()
+            memory.clear()
+            memory.addAll(
+                Regex("Program: (\\d+(?:,\\d+)*)")
+                    .matchEntire(input[4])!!
+                    .groupValues[1]
+                    .split(",")
+                    .map { it.toInt() }
+            )
+        }
+
+        fun process() {
+            val op = memory[pc++]
+            val param = memory[pc++]
+            when(op) {
+                0 -> adv(param)
+                1 -> bxl(param)
+                2 -> bst(param)
+                3 -> jnz(param)
+                4 -> bxc(param)
+                5 -> out(param)
+                6 -> bdv(param)
+                7 -> cdv(param)
+            }
+        }
+
+        private fun combo(param: Int) : Int = when(param) {
+            0 -> 0
+            1 -> 1
+            2 -> 2
+            3 -> 3
+            4 -> a
+            5 -> b
+            6 -> c
+            7 -> throw IllegalStateException("reserved")
+            else -> throw IllegalStateException("illegal value")
+        }
+        private fun cdv(param: Int) {
+            val denom = combo(param)
+            c = if (denom>31) 0 else a shr denom
+        }
+
+        private fun bdv(param: Int) {
+            val denom = combo(param)
+            b = if (denom>31) 0 else a shr denom
+        }
+
+        private fun out(param: Int) {
+            outValues += combo(param) and 7
+        }
+
+        private fun bxc(param: Int) {
+            b = b xor c
+        }
+
+        private fun jnz(param: Int) {
+            if (a != 0)
+                pc = param
+        }
+
+        private fun bst(param: Int) {
+            b = combo(param) and 7
+        }
+
+        private fun bxl(param: Int) {
+            b = b xor param
+        }
+
+        private fun adv(param: Int) {
+            val denom = combo(param)
+            a = if (denom>31) 0 else a shr denom
+        }
+    }
+    var proc = Processor()
+    fun part1(input: List<String>): String {
+        proc = Processor()
+        proc.readInput(input)
+        try {
+            while (true)
+                proc.process()
+        } catch (except : Exception) {
+            except.printStackTrace()
+        }
+        return proc.outValues.joinToString(",")
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        proc = Processor()
+        proc.readInput(input)
+        val firstB = proc.b
+        val firstC = proc.c
+        var firstA = -1
+        do {
+            proc.a = ++firstA
+            proc.pc = 0
+            proc.outValues.clear()
+            try {
+                while (true) {
+                    proc.process()
+                    for (pair in proc.outValues.zip(proc.memory))
+                        if (pair.first != pair.second) throw IllegalStateException("wrong output")
+                }
+            } catch (_ : Exception) { }
+        } while (proc.outValues != proc.memory)
+        return firstA
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
+    /*
+    part1(readInput("Day17_test1"))
+    check(proc.b == 1)
+    check(part1(readInput("Day17_test2")) == "0,1,2")
+    check(part1(readInput("Day17_test3")) == "4,2,5,6,7,7,7,7,3,1,0")
+    check(proc.a == 0)
+    part1(readInput("Day17_test4"))
+    check(proc.b == 26)
+    part1(readInput("Day17_test5"))
+    check(proc.b == 44354)
     val testInput = readInput("Day17_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == "4,6,3,5,6,3,5,2,1,0")
+*/
+    val testInput6 = readInput("Day17_test6")
+    check(part2(testInput6) == 117440)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day17")
+    println("Part1")
     part1(input).println()
+
+    // not 6,2,0,3,0,6,0,0,3
+    println("Part2")
     part2(input).println()
 }
