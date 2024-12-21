932c4da635a497b92aa7878f045007dd8619dfdc
Jeanpierre <laurent.jeanpierre@unicaen.fr>
2024-12-21 21:48:55 +0100

    feat: Day 21 - failed part2 dunno why

--- src/Day21Cache2.kt
+++ src/Day21Cache2.kt
@@ -1,5 +1,5 @@
-fun main() {
-    val targetsNum= mapOf(
+class Day21Cache2 {
+    private val targetsNum= mapOf(
         'A' to Pair(2,3),
         '0' to Pair(1,3),
         ' ' to Pair(0,3),
@@ -13,7 +13,7 @@ fun main() {
         '8' to Pair(1,0),
         '9' to Pair(2,0),
     )
-    val targetsPad= mapOf(
+    private val targetsPad= mapOf(
         'A' to Pair(2,0),
         '^' to Pair(1,0),
         ' ' to Pair(0,0),
@@ -21,10 +21,13 @@ fun main() {
         'v' to Pair(1,1),
         '>' to Pair(2,1),
     )
-    fun typeCode(code: String, effector: (String) -> String) : String {
+    private data class Context(val prev: Char, val current: Char)
+    private val cache = Array(26) { mutableMapOf<String, Long>() }
+
+    fun typeCode(code: String, depth: Int) : Long {
         var x = 2
         var y = 3
-        var total = ""
+        var total = 0L
         for (letter in code) {
             val target = targetsNum[letter]!!
             if (y == 3) {
@@ -48,7 +51,7 @@ fun main() {
                 }
                 manip.append('A')
                 if (target.first == 0 || target.first==px || target.second==py) {
-                    total += effector(manip.toString())
+                    total += compute(manip.toString(), depth)
                 } else {
                     val manip2 = StringBuilder()
                     while (target.first < px) {
@@ -67,9 +70,9 @@ fun main() {
                         check(px!=0 || py !=3)
                     }
                     manip2.append('A')
-                    val effector1 = effector(manip.toString())
-                    val effector2 = effector(manip2.toString())
-                    total += if (effector1.length < effector2.length) effector1 else effector2
+                    val effector1 = compute(manip.toString(), depth)
+                    val effector2 = compute(manip2.toString(), depth)
+                    total += if (effector1 < effector2) effector1 else effector2
                 }
             } else if (target.second == 3) {
                 val manip = StringBuilder()
@@ -92,7 +95,7 @@ fun main() {
                 }
                 manip.append('A')
                 if (px == 0 || target.first==px || target.second==py) {
-                    total += effector(manip.toString())
+                    total += compute(manip.toString(), depth)
                 } else {
                     val manip2 = StringBuilder()
                     while (target.second > py) {
@@ -111,9 +114,9 @@ fun main() {
                         check(px!=0 || py !=3)
                     }
                     manip2.append('A')
-                    val effector1 = effector(manip.toString())
-                    val effector2 = effector(manip2.toString())
-                    total += if (effector1.length < effector2.length) effector1 else effector2
+                    val effector1 = compute(manip.toString(), depth)
+                    val effector2 = compute(manip2.toString(), depth)
+                    total += if (effector1 < effector2) effector1 else effector2
                 }
             } else {
                 val manipX = StringBuilder()
@@ -139,20 +142,20 @@ fun main() {
                     check(x!=0 || y !=3)
                 }
                 if (manipX.isEmpty() || manipY.isEmpty())
-                    total += effector(manipX.toString()+manipY.toString()+'A')
+                    total += compute(manipX.toString()+manipY.toString()+'A', depth)
                 else {
-                    val lenXY = effector(manipX.toString() + manipY.toString() + 'A')
-                    val lenYX = effector(manipY.toString() + manipX.toString() + 'A')
-                    total += if (lenXY.length < lenYX.length) lenXY else lenYX
+                    val lenXY = compute(manipX.toString() + manipY.toString() + 'A', depth)
+                    val lenYX = compute(manipY.toString() + manipX.toString() + 'A', depth)
+                    total += if (lenXY < lenYX) lenXY else lenYX
                 }
             }
         }
         return total
     }
-    fun typePad(code: String, effector: (String)->String) : String {
-        var x=2
-        var y=0
-        var total = ""
+    private fun typePad(code: String, depth: Int, fromKey: Char) : Long {
+        var (x,y)=targetsPad[fromKey]!!
+
+        var total = 0L
         for (letter in code) {
             val target = targetsPad[letter]!!
             if (y == 0) {
@@ -173,7 +176,7 @@ fun main() {
                     check(x!=0 || y !=0)
                 }
                 manip.append('A')
-                total += effector(manip.toString())
+                total += compute(manip.toString(), depth)
             } else if (target.second == 0) {
                 val manip = StringBuilder()
                 while (target.first < x) {
@@ -192,7 +195,7 @@ fun main() {
                     check(x!=0 || y !=0)
                 }
                 manip.append('A')
-                total += effector(manip.toString())
+                total += compute(manip.toString(), depth)
             } else {
                 val manipX = StringBuilder()
                 val manipY = StringBuilder()
@@ -216,23 +219,26 @@ fun main() {
                     y--
                     check(x!=0 || y !=0)
                 }
-                val lenXY = effector(manipX.toString()+manipY.toString()+'A')
-                val lenYX = effector(manipY.toString()+manipX.toString()+'A')
-                total += if (lenXY.length < lenYX.length) lenXY else lenYX
+                val lenXY = compute(manipX.toString()+manipY.toString()+ 'A', depth)
+                val lenYX = compute(manipY.toString()+manipX.toString()+ 'A', depth)
+                total += if (lenXY < lenYX) lenXY else lenYX
             }
         }
         return total
     }
+    private fun compute(seq: String, depth: Int) : Long = if (depth==0) seq.length.toLong() else {
+        cache[depth].computeIfAbsent(seq) {
+            _ -> typePad(seq, depth-1, 'A')
+        }
+    }
+
+}
+fun main() {
     fun part1(input: List<String>): Long {
         var total = 0L
+        val puzzle = Day21Cache2()
         for (code in input) {
-            val len = typeCode(code) {
-                seq -> typePad(seq) {
-                    seq2 -> typePad(seq2) {
-                        seqF -> seqF
-                    }
-                }
-            }.also { it.println() }.length
+            val len = puzzle.typeCode(code, 2)//.also { it.println() }
             val value = code.dropLast(1).toInt()
             println("code -> $len * $value")
             total += value * len
@@ -240,36 +246,34 @@ fun main() {
         return total
     }
 
-    fun part2(input: List<String>): Long {
+    fun part2(input: List<String>, depth: Int): Long {
         var total = 0L
+        val puzzle = Day21Cache2()
         for (code in input) {
-            val len = typeCode(code) {
-                    seq1 -> typePad(seq1) {
-                    seq2 -> typePad(seq2) {
-                    seq3 -> typePad(seq3) {
-                    seq4 -> typePad(seq4) {
-                    //seq5 -> typePad(seq5) {
-                     seqF -> seqF
-                    //}
-                    }}}}
-            }.also { it.println() }.length
+            val len = puzzle.typeCode(code, depth)//.also { it.println() }
             val value = code.dropLast(1).toInt()
             println("code -> $len * $value")
             total += value * len
         }
         return total
     }
-
-    //check(part2(listOf("2"))>0L)// v<A<A>>^Av<<A>>^AvAA<^A>Av<A<AA>>^AvAA<^A>AAv<A>^AAv<<A>^A>AvA^Av<A<A>>^AvA<^A>Av<A<AA>>^AvA<^A>AvA^Av<A>^A<A>Av<A<A>>^AvA<^A>Av<<A>>^AvA^A
-
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day21_test")
     check(part1(testInput) == 126384L)
+    //check(part2(listOf("2"),4) == 0L) // 139 chars
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day21")
     println("Part1")
     part1(input).println() //197398 too low 210230 too high
-    part2(input).println()
+
+    println("Part2(2)")
+    check(part2(input, 2)==203814L)
+    println("Part2(5)")
+    check(part2(input,5)==3252306L)
+    println("Part2(15)")
+    check(part2(input,15)==35952774200L)
+    println("Part2(true one with cache)")
+    part2(input, 25).println() //157 436 062 802 508 too low (24 robots) < 399 669 673 013 954 too high
 }
--- src/Day21Int.kt
+++ src/Day21Int.kt
@@ -1,5 +1,5 @@
-fun main() {
-    val targetsNum= mapOf(
+class Day21Int {
+    private val targetsNum= mapOf(
         'A' to Pair(2,3),
         '0' to Pair(1,3),
         ' ' to Pair(0,3),
@@ -13,7 +13,7 @@ fun main() {
         '8' to Pair(1,0),
         '9' to Pair(2,0),
     )
-    val targetsPad= mapOf(
+    private val targetsPad= mapOf(
         'A' to Pair(2,0),
         '^' to Pair(1,0),
         ' ' to Pair(0,0),
@@ -21,10 +21,10 @@ fun main() {
         'v' to Pair(1,1),
         '>' to Pair(2,1),
     )
-    fun typeCode(code: String, effector: (String) -> String) : String {
+    fun typeCode(code: String, depth: Int, effector: (String,Int) -> Long) : Long {
         var x = 2
         var y = 3
-        var total = ""
+        var total = 0L
         for (letter in code) {
             val target = targetsNum[letter]!!
             if (y == 3) {
@@ -48,7 +48,7 @@ fun main() {
                 }
                 manip.append('A')
                 if (target.first == 0 || target.first==px || target.second==py) {
-                    total += effector(manip.toString())
+                    total += effector(manip.toString(), depth)
                 } else {
                     val manip2 = StringBuilder()
                     while (target.first < px) {
@@ -67,9 +67,9 @@ fun main() {
                         check(px!=0 || py !=3)
                     }
                     manip2.append('A')
-                    val effector1 = effector(manip.toString())
-                    val effector2 = effector(manip2.toString())
-                    total += if (effector1.length < effector2.length) effector1 else effector2
+                    val effector1 = effector(manip.toString(), depth)
+                    val effector2 = effector(manip2.toString(), depth)
+                    total += if (effector1 < effector2) effector1 else effector2
                 }
             } else if (target.second == 3) {
                 val manip = StringBuilder()
@@ -92,7 +92,7 @@ fun main() {
                 }
                 manip.append('A')
                 if (px == 0 || target.first==px || target.second==py) {
-                    total += effector(manip.toString())
+                    total += effector(manip.toString(), depth)
                 } else {
                     val manip2 = StringBuilder()
                     while (target.second > py) {
@@ -111,9 +111,9 @@ fun main() {
                         check(px!=0 || py !=3)
                     }
                     manip2.append('A')
-                    val effector1 = effector(manip.toString())
-                    val effector2 = effector(manip2.toString())
-                    total += if (effector1.length < effector2.length) effector1 else effector2
+                    val effector1 = effector(manip.toString(), depth)
+                    val effector2 = effector(manip2.toString(), depth)
+                    total += if (effector1 < effector2) effector1 else effector2
                 }
             } else {
                 val manipX = StringBuilder()
@@ -139,20 +139,20 @@ fun main() {
                     check(x!=0 || y !=3)
                 }
                 if (manipX.isEmpty() || manipY.isEmpty())
-                    total += effector(manipX.toString()+manipY.toString()+'A')
+                    total += effector(manipX.toString()+manipY.toString()+'A', depth)
                 else {
-                    val lenXY = effector(manipX.toString() + manipY.toString() + 'A')
-                    val lenYX = effector(manipY.toString() + manipX.toString() + 'A')
-                    total += if (lenXY.length < lenYX.length) lenXY else lenYX
+                    val lenXY = effector(manipX.toString() + manipY.toString() + 'A', depth)
+                    val lenYX = effector(manipY.toString() + manipX.toString() + 'A', depth)
+                    total += if (lenXY < lenYX) lenXY else lenYX
                 }
             }
         }
         return total
     }
-    fun typePad(code: String, effector: (String)->String) : String {
+    fun typePad(code: String, depth: Int, effector: (String, Int)->Long) : Long {
         var x=2
         var y=0
-        var total = ""
+        var total = 0L
         for (letter in code) {
             val target = targetsPad[letter]!!
             if (y == 0) {
@@ -173,7 +173,7 @@ fun main() {
                     check(x!=0 || y !=0)
                 }
                 manip.append('A')
-                total += effector(manip.toString())
+                total += effector(manip.toString(), depth)
             } else if (target.second == 0) {
                 val manip = StringBuilder()
                 while (target.first < x) {
@@ -192,7 +192,7 @@ fun main() {
                     check(x!=0 || y !=0)
                 }
                 manip.append('A')
-                total += effector(manip.toString())
+                total += effector(manip.toString(), depth)
             } else {
                 val manipX = StringBuilder()
                 val manipY = StringBuilder()
@@ -216,23 +216,22 @@ fun main() {
                     y--
                     check(x!=0 || y !=0)
                 }
-                val lenXY = effector(manipX.toString()+manipY.toString()+'A')
-                val lenYX = effector(manipY.toString()+manipX.toString()+'A')
-                total += if (lenXY.length < lenYX.length) lenXY else lenYX
+                val lenXY = effector(manipX.toString()+manipY.toString()+'A', depth)
+                val lenYX = effector(manipY.toString()+manipX.toString()+'A', depth)
+                total += if (lenXY < lenYX) lenXY else lenYX
             }
         }
         return total
     }
+    fun compute(seq: String, depth: Int) : Long = if (depth==0) seq.length.toLong() else typePad(seq, depth-1, this::compute)
+
+}
+fun main() {
     fun part1(input: List<String>): Long {
         var total = 0L
+        val puzzle = Day21Int()
         for (code in input) {
-            val len = typeCode(code) {
-                seq -> typePad(seq) {
-                    seq2 -> typePad(seq2) {
-                        seqF -> seqF
-                    }
-                }
-            }.also { it.println() }.length
+            val len = puzzle.typeCode(code, 2, puzzle::compute).also { it.println() }
             val value = code.dropLast(1).toInt()
             println("code -> $len * $value")
             total += value * len
@@ -240,28 +239,17 @@ fun main() {
         return total
     }
 
-    fun part2(input: List<String>): Long {
+    fun part2(input: List<String>, depth: Int): Long {
         var total = 0L
+        val puzzle = Day21Int()
         for (code in input) {
-            val len = typeCode(code) {
-                    seq1 -> typePad(seq1) {
-                    seq2 -> typePad(seq2) {
-                    seq3 -> typePad(seq3) {
-                    seq4 -> typePad(seq4) {
-                    //seq5 -> typePad(seq5) {
-                     seqF -> seqF
-                    //}
-                    }}}}
-            }.also { it.println() }.length
+            val len = puzzle.typeCode(code, depth, puzzle::compute)//.also { it.println() }
             val value = code.dropLast(1).toInt()
             println("code -> $len * $value")
             total += value * len
         }
         return total
     }
-
-    //check(part2(listOf("2"))>0L)// v<A<A>>^Av<<A>>^AvAA<^A>Av<A<AA>>^AvAA<^A>AAv<A>^AAv<<A>^A>AvA^Av<A<A>>^AvA<^A>Av<A<AA>>^AvA<^A>AvA^Av<A>^A<A>Av<A<A>>^AvA<^A>Av<<A>>^AvA^A
-
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day21_test")
@@ -271,5 +259,5 @@ fun main() {
     val input = readInput("Day21")
     println("Part1")
     part1(input).println() //197398 too low 210230 too high
-    part2(input).println()
+    part2(input, 15).println()
 }
